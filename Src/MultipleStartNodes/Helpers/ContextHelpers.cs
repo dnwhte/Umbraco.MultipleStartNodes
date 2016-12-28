b5fb6948a83610a4ca2025691f0dcbc5bfebbf11
Dan White <danwhite85@gmail.com>
2016-12-28 13:51:12 -0800

    Create a ContextHelpers class for ensuring Umbraco/Application contexts exists

---
+++ Src/MultipleStartNodes/Helpers/ContextHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using Umbraco.Core;
+using Umbraco.Core.Configuration;
+using Umbraco.Web;
+using Umbraco.Web.Routing;
+using Umbraco.Web.Security;
+
+namespace MultipleStartNodes.Helpers
+{
+    public class ContextHelpers
+    {
+        public static ApplicationContext EnsureApplicationContext()
+        {
+            if (ApplicationContext.Current != null)
+            {
+                return ApplicationContext.Current;
+            }
+
+            return ApplicationContext.EnsureContext(
+                ApplicationContext.Current, 
+                false
+            );
+        }
+
+        public static UmbracoContext EnsureUmbracoContext()
+        {
+            if (UmbracoContext.Current != null)
+            {
+                return UmbracoContext.Current;
+            }
+
+            var dummyHttpContext = new HttpContextWrapper(new HttpContext(new SimpleWorkerRequest("dummy.aspx", "", new StringWriter())));
+
+            return UmbracoContext.EnsureContext(
+                dummyHttpContext,
+                EnsureApplicationContext(),
+                new WebSecurity(dummyHttpContext, ApplicationContext.Current),
+                UmbracoConfig.For.UmbracoSettings(),
+                UrlProviderResolver.Current.Providers,
+                false);
+        }
+    }
+}
