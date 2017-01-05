5856c59a9204ad990b5b46d629031852eed397fe
Dan White <dawhite@lcsc.edu>
2017-01-04 16:43:14 -0800

    Create utilities js file

---
+++ Src/Site/App_Plugins/MultipleStartNodes/utilities.js
@@ -0,0 +1,16 @@
+﻿var multipleStartNodesUtilities = {
+    forceUmbTreeToUseStartNodes: function (template) {
+        template = template.replace(/<umb-tree\s/g, '<umb-tree customtreeparams="usestartnodes=true" ');
+
+        return template;
+    }
+
+    //// jquery way. For more complicated stuff
+    //forceUmbTreeToUseStartNodes: function (template) {
+    //    var $template = $(template);
+
+    //    $template.find('umb-tree').attr('customtreeparams', 'usestartnodes=true');
+
+    //    return $template[0].outerHTML;
+    //}
+};
